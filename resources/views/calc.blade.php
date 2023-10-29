b4d5de5c8dec326a8b97a16e18d832a2173567fb
maximus7022 <pash6790@gmail.com>
2023-10-29 22:24:54 +0200

    Changing text

--- resources/views/calc.blade.php
+++ resources/views/calc.blade.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html> <html lang="en"> <head>
-<meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport"
-    content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="{{asset('css/calc.css')}}">
-<title>Calculator 2.0</title>
-</head>
+<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="{{asset('css/calc.css')}}"> <title>Calculator</title> </head>
 
 <body>
     <section>
